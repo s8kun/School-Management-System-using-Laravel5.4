427c92c92dab66dbd747c16b44a43e51b0a33514
Abdullah Elferjani <itstd.6115@uob.edu.ly>
2026-04-24 02:37:07 +0200

    upgrade into 13x and delete the laravel tinker because doesn't support the verison 13x

--- Shcool management/config/app.php
+++ Shcool management/config/app.php
@@ -163,10 +163,7 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
 
-        /*
-         * Package Service Providers...
-         */
-        Laravel\Tinker\TinkerServiceProvider::class,
+
 
         /*
          * Application Service Providers...
