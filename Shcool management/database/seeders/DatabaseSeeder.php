b1148c97773fc16f8125b9b1ae3a8a61a70512fe
Abdullah Elferjani <itstd.6115@uob.edu.ly>
2026-04-24 02:52:31 +0200

    update seeder so it work with 13x now and delete the phpunit and download Pest

--- Shcool management/database/seeders/DatabaseSeeder.php
+++ Shcool management/database/seeders/DatabaseSeeder.php
@@ -1,8 +1,9 @@
 <?php
 
 use Illuminate\Database\Seeder;
-// use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -12,17 +13,50 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // Model::unguard();
+        Model::unguard();
 
-        // $this->call(AdminsTableSeeder::class);
-        // $this->call(LevelsTableSeeder::class);
-        // $this->call(ClassgroupsTableSeeder::class);
-        // $this->call(ClassroomsTableSeeder::class);
-        // $this->call(TeachersTableSeeder::class);
-        // $this->call(SubjectsTableSeeder::class);
-        // $this->call(StudentsTableSeeder::class);
-        // $this->call(StudentSubjectTableSeeder::class);
+        Schema::disableForeignKeyConstraints();
 
-        // Model::reguard();
+        foreach ([
+            'student_subject',
+            'users',
+            'subjects',
+            'students',
+            'teachers',
+            'classrooms',
+            'classgroups',
+            'levels',
+            'admins',
+        ] as $table) {
+            DB::table($table)->delete();
+        }
+
+        if (DB::connection()->getDriverName() === 'sqlite') {
+            DB::table('sqlite_sequence')
+                ->whereIn('name', [
+                    'users',
+                    'subjects',
+                    'students',
+                    'teachers',
+                    'classrooms',
+                    'classgroups',
+                    'levels',
+                    'admins',
+                ])
+                ->delete();
+        }
+
+        Schema::enableForeignKeyConstraints();
+
+        $this->call(AdminsTableSeeder::class);
+        $this->call(LevelsTableSeeder::class);
+        $this->call(ClassgroupsTableSeeder::class);
+        $this->call(ClassroomsTableSeeder::class);
+        $this->call(TeachersTableSeeder::class);
+        $this->call(SubjectsTableSeeder::class);
+        $this->call(StudentsTableSeeder::class);
+        $this->call(StudentSubjectTableSeeder::class);
+
+        Model::reguard();
     }
 }
